9b4c44b3b11f41d3b51b5f7cc64b0c68d0166d58
Rohit Sthapit <nhuzaa@gmail.com>
2024-07-07 16:15:31 -0400

    socials

---
+++ app/components/socials.tsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+const SocialLinks = () => {
+  // List of social media URLs you want to include
+  const socialUrls = [
+    'https://www.linkedin.com/in/nhuzaa/',
+    'https://www.twitter.com/nhuzaa',
+    'https://www.instagram.com/__nhuzaa',
+    'https://www.github.com/nhuzaa',
+    'https://stackoverflow.com/users/7104332/rohit-sthapit',
+  ];
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+      {socialUrls.map((url, index) => (
+        <SocialIcon key={index} url={url} fgColor="#ffffff"  bgColor="#333333" target="_blank"/>
+      ))}
+    </div>
+  );
+};
+
+export default SocialLinks;
